9009ec9402d330b1d01f646c36c58c169b84312e
yusuf hammadi <ucup1745@gmail.com>
2020-09-24 14:35:19 +0700

    Pertemuan1_Intent

---
+++ app/src/main/java/com/e/intent/MainActivity.java
@@ -0,0 +1,50 @@
+package com.e.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn);
+        btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveWithDataActivity = findViewById(R.id.btn1);
+        btnMoveWithDataActivity.setOnClickListener(this);
+
+        Button btnDialPhone = findViewById(R.id.btn_dial_num);
+        btnDialPhone.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.btn1:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Septian Nugraha");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NIM, "21120118130078");
+                startActivity(moveWithDataIntent);
+                break;
+            case R.id.btn_dial_num:
+                String phoneNumber = "087782804045";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
+
+        }
+    }
+
+}
